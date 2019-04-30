1834ed728a6b447496b07ed08afbe7cd9c73e745
Kristian Andersen <kristian1711@gmail.com>
2019-04-30 11:12:01 +0200

    Began ProductDAO Began first attempt of ProductDTO (still open for changes)

--- src/main/java/DAO/ProductDAO.java
+++ src/main/java/DAO/ProductDAO.java
@@ -2,16 +2,38 @@ package DAO;
 
 import model.ProductDTO;
 
+import java.sql.*;
+
 public class ProductDAO implements IProductDAO {
 
-    private final String dbURL = "ec2-52-30-211-3.eu-west-1.compute.amazonaws.com";
+    private final String dbURL = "jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s180943?";
     private final String dbName = "s180943";
     private final String dbPass = "UXZTadQzbPrlIosGCZYNF";
 
+    private Connection getConnection() throws DALException {
+        try {
+            return DriverManager.getConnection(dbURL, dbName, dbPass);
+        } catch(SQLException e) {
+            throw new DALException(e.getMessage());
+        }
+    }
     //Create
+
     @Override
     public void createProduct(ProductDTO pro) throws DALException {
+        try(Connection con = getConnection()) {
+            String query = "INSERT INTO Product VALUES(?,?,?,?,?)";
+            PreparedStatement statement = con.prepareStatement(query);
 
+            //TODO Set into statement
+            statement.setInt(1, pro.getID());
+
+
+            statement.execute();
+
+        } catch (SQLException e) {
+            throw new DALException(e.getMessage());
+        }
     }
 
     //Read
