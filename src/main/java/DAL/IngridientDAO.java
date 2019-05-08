33a009f8f1a9185f3fad79be57029df3a04a4845
AndreasBGJensen <bgandreas@hotmail.com>
2019-05-08 08:37:38 +0200

    Commit: Before merging with RecipeDAo for testing connection

--- src/main/java/DAL/IngridientDAO.java
+++ src/main/java/DAL/IngridientDAO.java
@@ -7,27 +7,26 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class IngridientDAO implements IIngridientDAO {
-
-    private DBConnection conn = new DBConnection();
+     DBConnection conn = new DBConnection();
 
     public IngridientDAO(){}
 
-    private Connection createConnection() throws SQLException {
+   /* private Connection createConnection() throws SQLException {
         return  DriverManager.getConnection("jdbc:mysql://ec2-52-30-211-3.eu-west-1.compute.amazonaws.com/s180943?"
                 + "UXZTadQzbPrlIosGCZYNF");
-    }
+    }*/
 
 
     public void createIngridient(IIngridientDTO ingridient) throws DALException {
 
-        try(Connection connection = createConnection()) {
+        try(Connection connection = conn.createConnection()) {
 
             String sql = "INSERT INTO ingredient(ingredient_id,ingredient_name,needs_refill)" + "VALUES(?,?,?)";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, ingridient.getIngredient_id());
             statement.setString(2, ingridient.getIngredient_name());
             statement.setBoolean(3, ingridient.getNeeds_refill());
-            statement.executeUpdate();
+            statement.executeQuery();
 
         }catch (SQLException e) {
             throw new DALException("Could not create the ingredient");
@@ -43,7 +42,7 @@ public class IngridientDAO implements IIngridientDAO {
             statement.setString(1, ingridient.getIngredient_name());
             statement.setBoolean(2, ingridient.getNeeds_refill());
             statement.setInt(3,ingridient.getIngredient_id());
-            statement.executeUpdate();
+            statement.executeQuery();
 
         }catch (SQLException e){e.getErrorCode();}
             throw new DALException("Could not update");
@@ -57,7 +56,7 @@ public class IngridientDAO implements IIngridientDAO {
             String sql = "DELETE FROM ingredient WHERE ingredient_id=?";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setInt(1, ingridient_id);
-            statement.executeUpdate();
+            statement.executeQuery();
 
         }catch (SQLException e){
             throw new DALException("Could not delete the ingredient");
