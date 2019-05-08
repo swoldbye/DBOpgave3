63d1bc2d3ba839be9f63db89e6b57e7908cf1886
AndreasBGJensen <bgandreas@hotmail.com>
2019-05-08 14:44:08 +0200

    Commit: Java doc

--- src/main/java/DAL/IngridientDAO.java
+++ src/main/java/DAL/IngridientDAO.java
@@ -2,58 +2,69 @@ package DAL;
 
 import DTO.IIngridientDTO;
 import DTO.IngridientDTO;
-import sun.rmi.server.InactiveGroupException;
 
 import java.sql.*;
 import java.util.ArrayList;
 
+
+
 public class IngridientDAO implements IIngridientDAO {
     DBConnection conn = new DBConnection();
 
-    //public IngridientDAO(){}
 
-    /*private Connection createConnection() throws SQLException {
-        return  DriverManager.getConnection("jdbc:mysql://localhost:3306/root?"
-                + "Firma2018");
-    }*/
+    /**
+     * Inserts an object into the table.
+     * @param ingridient
+     * @throws DALException
+     */
 
+        public void createIngridient(IIngridientDTO ingridient) throws DALException {
 
-    public void createIngridient(IIngridientDTO ingridient) throws DALException {
+            try (Connection connection = conn.createConnection()) {
 
-        try (Connection connection = conn.createConnection()) {
+                String sql = "INSERT INTO ingredient(ingredient_id,ingredient_name,needs_refill)" + "VALUES(?,?,?)";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setInt(1, ingridient.getIngredient_id());
+                statement.setString(2, ingridient.getIngredient_name());
+                statement.setBoolean(3, ingridient.getNeeds_refill());
+                statement.execute();
 
-            String sql = "INSERT INTO ingredient(ingredient_id,ingredient_name,needs_refill)" + "VALUES(?,?,?)";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setInt(1, ingridient.getIngredient_id());
-            statement.setString(2, ingridient.getIngredient_name());
-            statement.setBoolean(3, ingridient.getNeeds_refill());
-            statement.execute();
-
-        } catch (SQLException e) {
-            throw new DALException(e.getMessage());
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage());
+            }
         }
 
-    }
+    /**
+     * Method updates an object by updating the row whitch is defined by the ingredient_id.
+     * @param ingridientDTO
+     * @return
+     * @throws DALException
+     */
 
-    public IIngridientDTO updateIngridient(IIngridientDTO ingridient) throws DALException {
-        try (Connection connection = conn.createConnection()) {
+        public IIngridientDTO updateIngridient(IIngridientDTO ingridientDTO) throws DALException {
+            try (Connection connection = conn.createConnection()) {
 
-            String sql = "UPDATE ingredient SET ingredient_name = ?, needs_refill=?  WHERE ingredient_id = ?";
-            PreparedStatement statement = connection.prepareStatement(sql);
-            statement.setString(1, ingridient.getIngredient_name());
-            statement.setBoolean(2, ingridient.getNeeds_refill());
-            statement.setInt(3, ingridient.getIngredient_id());
-            statement.execute();
+                String sql = "UPDATE ingredient SET ingredient_name = ?, needs_refill=?  WHERE ingredient_id = ?";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, ingridientDTO.getIngredient_name());
+                statement.setBoolean(2, ingridientDTO.getNeeds_refill());
+                statement.setInt(3, ingridientDTO.getIngredient_id());
+                statement.execute();
 
-        } catch (SQLException e) {
-            throw new DALException(e.getMessage());
-
+            } catch (SQLException e) {
+                throw new DALException(e.getMessage());
+            }
+            return null;
         }
-        return null;
-    }
 
+    /**
+     * This method will not be used, because we do not want to delete any ingrediens permanent from the database.
+     * We want to be able to see all the ingredients that have been used in old as well as new recipes.
+     * @param ingridient_id
+     * @throws DALException
+     */
 
-    public void deleteIngridient ( int ingridient_id) throws DALException {
+        public void deleteIngridient ( int ingridient_id) throws DALException {
 
             try (Connection connection = conn.createConnection()) {
 
@@ -65,16 +76,18 @@ public class IngridientDAO implements IIngridientDAO {
             } catch (SQLException e) {
                 throw new DALException(e.getMessage());
             }
-
         }
 
+    /**
+     * Method returns a object of IngredientDTO.
+     * @param ingredient_id
+     * @return IIngridientDTO
+     * @throws DALException
+     */
 
         public IIngridientDTO getIngredient ( int ingredient_id)throws DALException {
 
-
             try (Connection connection = conn.createConnection()) {
-
-                //I do npt use PreparedStatement because data is selectet and not updated.
 
                 String sql = "SELECT * FROM ingredient WHERE ingredient_id = ?";
                 PreparedStatement statement = connection.prepareStatement(sql);
@@ -99,62 +112,30 @@ public class IngridientDAO implements IIngridientDAO {
         }
 
 
+    /**
+     *Method gets a list of all the ingredients that is used by any recipes.
+     * @return A list that contains alle ingredients (id, names, and needs refill attribut)
+     * @throws DALException
+     */
+
+          public ArrayList<IIngridientDTO> getIngredientList() throws DALException{
 
 
-  public ArrayList<IIngridientDTO> getIngredientList() throws DALException{
+                ArrayList<IIngridientDTO> ingredintList = new ArrayList<>();
+                String sql ="SELECT * FROM ingredient";
+                try (Connection connection = conn.createConnection()) {
 
+                    PreparedStatement statement = connection.prepareStatement(sql);
+                    ResultSet result = statement.executeQuery();
 
-        ArrayList<IIngridientDTO> ingredintList = new ArrayList<>();
-        String sql ="SELECT * FROM ingredient";
-        try (Connection connection = conn.createConnection()) {
+                    while(result.next()) {
+                        IIngridientDTO  ingredient = new IngridientDTO(result.getInt(1),result.getString(2),result.getBoolean(3));
+                        ingredintList.add(ingredient);
+                    }
+                    return ingredintList;
 
-            PreparedStatement statement = connection.prepareStatement(sql);
-            ResultSet result = statement.executeQuery();
-
-
-            while(result.next()) {
-                IIngridientDTO  ingredient = new IngridientDTO(result.getInt(1),result.getString(2),result.getBoolean(3));
-                ingredintList.add(ingredient);
+                } catch (SQLException e) {
+                    throw new DALException("hej");
+                }
             }
-            return ingredintList;
-
-        } catch (SQLException e) {
-
-            throw new DALException("hej");
-            //Remember to handle Exceptions gracefully! Connection might be Lost....
-        }
-
-    }
-
-
-        public static void main (String[]args) throws DALException {
-            IIngridientDTO ingridien = new IngridientDTO(100, "dsadsadasdsfdsfsdf", true);
-            IIngridientDTO ingridien2 = new IngridientDTO(100, "dsadsadasdsfdsfsdf", false);
-
-
-/*
-            one.createIngridient(ingridien);
-            one.deleteIngridient(1);*/
-            IngridientDAO one = new IngridientDAO();
-
-            //System.out.println(one.getIngredient(2));
-
-            //System.out.println(one.getIngredient(1).toString());
-            one.createIngridient(ingridien);
-            System.out.println(one.getIngredient(100).toString());
-            one.updateIngridient(ingridien2);
-            System.out.println(one.getIngredient(100).toString());
-
-            /*ArrayList<IIngridientDTO> list = one.getIngredientList();
-
-            for(int i = 0; i< list.size();i++){
-                System.out.println(list.get(i).toString());
-            }*/
-
-
-
-
-        }
-
-
 }
